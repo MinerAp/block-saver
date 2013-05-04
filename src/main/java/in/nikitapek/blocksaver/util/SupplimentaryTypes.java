6a5b67474faa3c59d7e4eb50e3d1f6b4e12d8dfc
Nikita Pekin <nikitapekin@gmail.com>
2013-05-04 17:10:37 -0400

    Removed excess comment

--- src/main/java/in/nikitapek/blocksaver/util/SupplimentaryTypes.java
+++ src/main/java/in/nikitapek/blocksaver/util/SupplimentaryTypes.java
@@ -12,6 +12,5 @@ public final class SupplimentaryTypes {
 
     public static final Type BLOCK = new TypeToken<Block>() {}.getType();
     public static final Type BYTE = new TypeToken<Byte>() {}.getType();
-    //public static final Type BOOLEAN = new TypeToken<Boolean>() {}.getType();
     public static final Type MATERIAL = new TypeToken<Material>() {}.getType();
 }
