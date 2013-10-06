15debcacbba7c2363c2b0483e5b4aa53c6728e3e
Nikita Pekin <nikitapekin@gmail.com>
2013-10-06 00:33:30 -0400

    Remove unused reinforcementManager field in BlockSaverPlugin

--- src/main/java/in/nikitapek/blocksaver/BlockSaverPlugin.java
+++ src/main/java/in/nikitapek/blocksaver/BlockSaverPlugin.java
@@ -8,13 +8,9 @@ import in.nikitapek.blocksaver.util.BlockSaverConfigurationContext;
 import com.amshulman.mbapi.MbapiPlugin;
 
 public final class BlockSaverPlugin extends MbapiPlugin {
-    public ReinforcementManager reinforcementManager;
-
     @Override
     public void onEnable() {
         final BlockSaverConfigurationContext configurationContext = new BlockSaverConfigurationContext(this);
-
-        reinforcementManager = configurationContext.getReinforcementManager();
 
         registerCommandExecutor(new CommandBlockSaver(configurationContext));
         registerEventHandler(new BlockSaverListener(configurationContext));
