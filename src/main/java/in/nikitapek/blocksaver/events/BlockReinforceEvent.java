03573dee150d59500ef69dbc2c0743719e86ea48
Nikita Pekin <nikitapekin@gmail.com>
2013-12-06 00:29:51 -0500

    Fix handlers and turn Events into BlockEvents

--- src/main/java/in/nikitapek/blocksaver/events/BlockReinforceEvent.java
+++ src/main/java/in/nikitapek/blocksaver/events/BlockReinforceEvent.java
@@ -1,13 +1,13 @@
 package in.nikitapek.blocksaver.events;
 
 import org.bukkit.block.Block;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.block.BlockEvent;
 
-public class BlockReinforceEvent extends Event {
+public class BlockReinforceEvent extends BlockEvent {
+    private static final HandlerList handlers = new HandlerList();
 
     protected long time;
-    protected Block reinforcedBlock;
     protected String playerName;
     protected boolean isLogged;
 
@@ -16,18 +16,14 @@ public class BlockReinforceEvent extends Event {
     }
 
     public BlockReinforceEvent(Long time, Block reinforcedBlock, String playerName, boolean isLogged) {
+        super(reinforcedBlock);
         this.time = time;
-        this.reinforcedBlock = reinforcedBlock;
         this.playerName = playerName;
         this.isLogged = isLogged;
     }
 
     public Long getTime() {
         return time;
-    }
-
-    public Block getBlock() {
-        return reinforcedBlock;
     }
 
     public String getPlayerName() {
@@ -40,6 +36,10 @@ public class BlockReinforceEvent extends Event {
 
     @Override
     public HandlerList getHandlers() {
-        return null;
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
